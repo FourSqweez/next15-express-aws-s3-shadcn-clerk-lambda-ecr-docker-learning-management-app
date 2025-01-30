9e9e53290344dee0911ce6a49bc245327a1a920c
Foursqweez <jeerasak.code@gmail.com>
2025-01-30 17:37:28 +0700

    Setup backend libs and utils from the course

---
+++ server/src/index.ts
@@ -0,0 +1,38 @@
+import bodyParser from "body-parser"
+import cors from "cors"
+import dotenv from "dotenv"
+import * as dynamoose from "dynamoose"
+import express from "express"
+import helmet from "helmet"
+import morgan from "morgan"
+
+// configurations
+dotenv.config()
+
+const isProduction = process.env.NODE_ENV === "production"
+
+if (!isProduction) {
+  dynamoose.aws.ddb.local()
+}
+
+const app = express()
+app.use(express.json()) // for parsing application/json
+app.use(cors()) // for cross origin requests
+app.use(helmet()) // for security
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })) // for security
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
+app.use(morgan("common")) // for logging
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+// Start server
+const port = process.env.PORT || 8001
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
+  })
+}
