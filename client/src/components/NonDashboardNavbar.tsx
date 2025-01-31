d67c92ee11247bd421cc027499fe2608eb178073
Foursqweez <jeerasak.code@gmail.com>
2025-01-31 16:07:30 +0700

    Update html layout

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -22,13 +22,13 @@ const NonDashboardNavbar = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="nondashboard-navbar__actions">
-        <button className="nondashboard-navbar__notification-button">
-          <span className="nondashboard-navbar__notification-indicator"></span>
-          <Bell className="nondashboard-navbar__notification-icon" />
-        </button>
-        {/* Sign in buttons */}
+        <div className="nondashboard-navbar__actions">
+          <button className="nondashboard-navbar__notification-button">
+            <span className="nondashboard-navbar__notification-indicator"></span>
+            <Bell className="nondashboard-navbar__notification-icon" />
+          </button>
+          {/* Sign in buttons */}
+        </div>
       </div>
     </nav>
   )
