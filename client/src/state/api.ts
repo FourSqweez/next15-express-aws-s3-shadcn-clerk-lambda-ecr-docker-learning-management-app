4fdf85f5d559e903fd00c5a0f05c1052837b8d07
Foursqweez <jeerasak.code@gmail.com>
2025-01-31 13:11:59 +0700

    Create RTK-query to fetch the data and Create the customBaseQuery

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,46 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const customBaseQuery = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: object,
+) => {
+  const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  })
+
+  try {
+    const result = await baseQuery(args, api, extraOptions)
+    if (result.data) {
+      result.data = (result.data as { data: any }).data
+    }
+    return result
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error"
+
+    return { error: { status: "FETCH_ERROR", error: errorMessage } }
+  }
+}
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+  baseQuery: customBaseQuery,
   reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
-});
+  tagTypes: ["Courses"],
+  endpoints: (build) => ({
+    getCourses: build.query<Course[], { category?: string }>({
+      query: ({ category }) => ({
+        url: "courses",
+        params: { category },
+      }),
+      providesTags: ["Courses"],
+    }),
+    getCourse: build.query<Course, string>({
+      query: (id) => `courses/${id}`,
+      providesTags: (result, error, id) => [{ type: "Courses", id }],
+    }),
+  }),
+})
 
-export const {} = api;
+export const { useGetCoursesQuery, useGetCourseQuery } = api
