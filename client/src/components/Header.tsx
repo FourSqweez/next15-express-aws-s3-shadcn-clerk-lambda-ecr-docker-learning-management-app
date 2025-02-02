cf019d441368aec598b19b1cf2aec8f18620c6b8
Foursqweez <jeerasak.code@gmail.com>
2025-02-02 16:20:37 +0700

    Create Sidebar for profile page

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = ({ title, subtitle, rightElement }: HeaderProps) => {
+  return (
+    <div className="header">
+      <div>
+        <h1 className="header__title">{title}</h1>
+        <p className="header__subtitle">{subtitle}</p>
+        {rightElement && <div>{rightElement}</div>}
+      </div>
+    </div>
+  )
+}
+
+export default Header
