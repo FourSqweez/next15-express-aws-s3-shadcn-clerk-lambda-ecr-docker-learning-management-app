05fdf848bb5e57110418f62fbd7c4fadbd04e933
Foursqweez <jeerasak.code@gmail.com>
2025-02-02 17:39:14 +0700

    Setup clerkClient for update the user setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { createClerkClient } from "@clerk/express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -6,6 +7,7 @@ import express from "express"
 import helmet from "helmet"
 import morgan from "morgan"
 import courseRoutes from "./routes/courseRoutes"
+import userClerkRoutes from "./routes/userClerkRoutes"
 
 // configurations
 dotenv.config()
@@ -15,6 +17,10 @@ const isProduction = process.env.NODE_ENV === "production"
 if (!isProduction) {
   dynamoose.aws.ddb.local()
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+})
 
 const app = express()
 app.use(express.json()) // for parsing application/json
@@ -31,6 +37,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/courses", courseRoutes)
+app.use("/user/clerk", userClerkRoutes)
 
 // Start server
 const port = process.env.PORT || 8001
